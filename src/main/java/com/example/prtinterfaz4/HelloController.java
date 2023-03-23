609f05ec82b8789369c536d6771bee47ddc9a46f
PritOnce11 <pritonce@gmail.com>
2023-03-23 17:41:17 +0100

    suuu

--- src/main/java/com/example/prtinterfaz4/HelloController.java
+++ src/main/java/com/example/prtinterfaz4/HelloController.java
@@ -3,6 +3,7 @@ package com.example.prtinterfaz4;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -68,6 +69,80 @@ public class HelloController implements Initializable {
             alert.setTitle("Error");
             alert.setContentText("Formato incorrecto");
             alert.showAndWait();
+        }
+    }
+
+    public void elegir (Event event){
+        Persona p = this.tabla.getSelectionModel().getSelectedItem();
+        if(p != null){
+            this.fieldNombre.setText(p.getNombre());
+            this.fieldApellidos.setText(p.getApellidos());
+            this.fieldEdad.setText(p.getEdad()+"");
+        }
+    }
+    public void modificarPersona(Event event){
+        Persona p = this.tabla.getSelectionModel().getSelectedItem();
+
+        if(p == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("Debes seleccionar una persona");
+            alert.showAndWait();
+        }else {
+            try {
+                String name = this.fieldNombre.getText();
+                String surname = this.fieldApellidos.getText();
+                int age = Integer.parseInt(this.fieldEdad.getText());
+                Persona p2 = new Persona(name,surname,age);
+
+                if(!this.personas.contains(p2)){
+                    p.setNombre(p2.getNombre());
+                    p.setApellidos(p2.getApellidos());
+                    p.setEdad(p2.getEdad());
+
+                    this.tabla.refresh();
+
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setHeaderText(null);
+                    alert.setTitle("Info");
+                    alert.setContentText("Persona modificada");
+                    alert.showAndWait();
+                }else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setHeaderText(null);
+                    alert.setTitle("Error");
+                    alert.setContentText("La persona existe");
+                    alert.showAndWait();
+                }
+
+            } catch (NumberFormatException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void eliminarPersona(){
+        Persona p = this.tabla.getSelectionModel().getSelectedItem();
+
+        if (p == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("Debes seleccionar una persona");
+            alert.showAndWait();
+        } else {
+
+            // La elimino de la lista
+            this.personas.remove(p);
+            // Refresco la lista
+            this.tabla.refresh();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText(null);
+            alert.setTitle("Info");
+            alert.setContentText("Persona eliminada");
+            alert.showAndWait();
+
         }
     }
 }
